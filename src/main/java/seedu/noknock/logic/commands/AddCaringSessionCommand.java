0601e6b58b42da28ad123a82c10eb2365a2c1394
xiaoqi01010 <97711145+xiaoqi01010@users.noreply.github.com>
2025-10-19 19:48:48 +0800

    Add AddCaringSessionCommand to support adding caring sessions for patients

---
+++ src/main/java/seedu/noknock/logic/commands/AddCaringSessionCommand.java
@@ -0,0 +1,104 @@
+package seedu.noknock.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.noknock.logic.parser.CliSyntax.PREFIX_DATE;
+import static seedu.noknock.logic.parser.CliSyntax.PREFIX_NOTES;
+import static seedu.noknock.logic.parser.CliSyntax.PREFIX_TIME;
+import static seedu.noknock.logic.parser.CliSyntax.PREFIX_TYPE;
+import static seedu.noknock.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import seedu.noknock.commons.core.index.Index;
+import seedu.noknock.commons.util.ToStringBuilder;
+import seedu.noknock.logic.Messages;
+import seedu.noknock.logic.commands.exceptions.CommandException;
+import seedu.noknock.model.Model;
+import seedu.noknock.model.person.Patient;
+import seedu.noknock.model.session.CaringSession;
+
+/**
+ * Adds a caring session to an existing patient
+ */
+public class AddCaringSessionCommand extends Command {
+    public static final String COMMAND_WORD = "add-nok";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Adds a care task for a patient .\n"
+            + "Parameters: PATIENT_INDEX (must be a positive integer) "
+            + PREFIX_DATE + "DATE "
+            + PREFIX_TIME + "TIME "
+            + PREFIX_TYPE + "CARE_TYPE ["
+            + PREFIX_NOTES + "notes] "
+            + "Example: " + COMMAND_WORD + " 1 "
+            + PREFIX_DATE + "2024-12-25 "
+            + PREFIX_TIME + "14:30 "
+            + PREFIX_TYPE + "medication "
+            + PREFIX_NOTES + "Give insulin shot ";
+
+    public static final String MESSAGE_ADD_CARING_SESSION_SUCCESS = "Added Caring Session: %1$s to Patient: %2$s";
+    public static final String MESSAGE_INVALID_SESSION = "This session: %1$s is invalid";
+
+    private final Index patientIndex;
+    private final CaringSession sessionToAdd;
+
+    /**
+     * Creates a command which adds a {@Link CaringSession} to an existing patient
+     * @param patientIndex Index of the patient in the filtered patient list.
+     * @param sessionToAdd specifies the caring session to be added.
+     */
+    public AddCaringSessionCommand(Index patientIndex, CaringSession sessionToAdd) {
+        requireNonNull(patientIndex);
+        requireNonNull(sessionToAdd);
+        this.patientIndex = patientIndex;
+        this.sessionToAdd = sessionToAdd;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Patient> patientList = model.getFilteredPatientList();
+
+        if (patientIndex.getZeroBased() >= patientList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        Patient patient = patientList.get(patientIndex.getZeroBased());
+        List<CaringSession> caringSessionList = patient.getCaringSessionList();
+
+        if (caringSessionList.contains(sessionToAdd)) {
+            throw new CommandException(MESSAGE_INVALID_SESSION);
+        }
+
+        List<CaringSession> updatedCaringSessionList = new ArrayList<>(caringSessionList);
+        updatedCaringSessionList.add(sessionToAdd);
+
+        Patient editedPatient = patient.withCaringSessionList(updatedCaringSessionList);
+        model.setPatient(patient, editedPatient);
+        model.updateFilteredPatientList(PREDICATE_SHOW_ALL_PERSONS);
+
+        return new CommandResult(String.format(MESSAGE_ADD_CARING_SESSION_SUCCESS,
+                sessionToAdd.toString(), Messages.format(editedPatient)));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if (!(other instanceof AddCaringSessionCommand otherAddCommand)) {
+            return false;
+        }
+        return patientIndex.equals(otherAddCommand.patientIndex)
+                && sessionToAdd.equals(otherAddCommand.sessionToAdd);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("patientIndex", patientIndex)
+                .add("sessionToAdd", sessionToAdd)
+                .toString();
+    }
+}
--- src/main/java/seedu/noknock/logic/parser/CliSyntax.java
+++ src/main/java/seedu/noknock/logic/parser/CliSyntax.java
@@ -9,10 +9,11 @@ public class CliSyntax {
     public static final Prefix PREFIX_NAME = new Prefix("n/");
     public static final Prefix PREFIX_IC = new Prefix("ic/");
     public static final Prefix PREFIX_PHONE = new Prefix("p/");
-    public static final Prefix PREFIX_EMAIL = new Prefix("e/");
-    public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
     public static final Prefix PREFIX_WARD = new Prefix("w/");
     public static final Prefix PREFIX_RELATIONSHIP = new Prefix("r/");
-
+    public static final Prefix PREFIX_TIME = new Prefix("time/");
+    public static final Prefix PREFIX_TYPE = new Prefix("type/");
+    public static final Prefix PREFIX_DATE = new Prefix("d/");
+    public static final Prefix PREFIX_NOTES = new Prefix("notes/");
 }
